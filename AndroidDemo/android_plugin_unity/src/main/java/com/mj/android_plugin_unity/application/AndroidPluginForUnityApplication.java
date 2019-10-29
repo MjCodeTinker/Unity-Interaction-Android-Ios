028c17751e269ec21bc28981f00e641de86dae25
miaojian <miaojian@cmcm.com>
2019-10-29 19:15:17 +0800

    【Add】增加 ignore文件，AndroidPlugin 增加Appication  并且做验证
    【描述】:
    【影响范围(模块)】:无
    【测试建议】:无
    【自测通过】:通过
    【风险(可选)】:无
    【reviewed by】:self

--- AndroidDemo/android_plugin_unity/src/main/java/com/mj/android_plugin_unity/application/AndroidPluginForUnityApplication.java
+++ AndroidDemo/android_plugin_unity/src/main/java/com/mj/android_plugin_unity/application/AndroidPluginForUnityApplication.java
@@ -1,4 +1,23 @@
 package com.mj.android_plugin_unity.application;
 
-public class AndroidPluginForUnityApplication {
+import android.app.Application;
+import android.content.Context;
+import android.util.Log;
+
+public class AndroidPluginForUnityApplication extends Application {
+
+
+    private static final String TAG = "ForUnityApp";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.i(TAG,"onCreate()");
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        Log.i(TAG,"attachBaseContext()");
+    }
 }
